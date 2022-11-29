378d007daec21e45129885cfc44c8deabf2cf77f
James Anwyl <ja.anwyl@gmail.com>
2022-11-29 00:41:39 +0000

    Add axe-core and create logging task

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(message)
+      
+          return null
+        },
+      })
     },
     env: {
       formUrl: 'https://share.hsforms.com/1-79TdZVpS9igd9mdtdea5w1khjs'
